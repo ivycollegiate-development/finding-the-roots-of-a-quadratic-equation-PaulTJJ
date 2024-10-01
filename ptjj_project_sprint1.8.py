46f60164a845bd5f7ea9c02fd4c0069ec68120fb
PaulTJJ <pjones25_student@ivycollegiate.org>
2024-10-01 05:47:03 +0000

    jun

---
+++ ptjj_project_sprint1.8.py
@@ -0,0 +1,8 @@
+def calculate_discriminant(a, b, c):
+    return b**2 - 4*a*c
+print(calculate_discriminant(1, -5, 6))
+import math
+def calculate_roots(a, b, c):
+print(calculate_roots(1, -5, 6))
+print(calculate_roots(1, 2, 1))
+##joe mama
