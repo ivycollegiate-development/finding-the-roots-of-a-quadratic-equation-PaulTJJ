661037502d21cc342197266ad4d28bd20a3a2ca7
PaulTJJ <pjones25_student@ivycollegiate.org>
2024-10-01 06:21:10 +0000

    calculate_discriminant

--- ptjj_project_sprint1.8.py
+++ ptjj_project_sprint1.8.py
@@ -1,6 +1,7 @@
 def calculate_discriminant(a, b, c):
     return b**2 - 4*a*c
 print(calculate_discriminant(1, -5, 6))
+else if calculate_discriminant = 0;
 import math
 def calculate_roots(a, b, c):
 print(calculate_roots(1, -5, 6))
